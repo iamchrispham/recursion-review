f0c0ed5d3fd5186e6ebb5dc1ef1651624494062f
iamchrispham <chrismpham@gmail.com>
2019-05-28 15:54:25 -0700

    Completed source code and specs for getElementByClassName and stringifyJSON

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,25 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
+var getElementsByClassName = function(className, node
 ) {
-  // your code here
+  // declaring node as either the second argument if given, 
+  // or as document.body (the top of the HTML)
+  node = node || document.body
+  var nodes = [];
+  // declare a variable to represent all class names, and split
+  // each class name
+  var allClassNames = node.className.split(' ');
+  // if it is not -1 (meaning nonexistent), push existing node into nodes array
+  if (allClassNames.indexOf(className) !== -1) {
+    nodes.push(node);
+  }
+
+  // iterate through child nodes, as they also may have className
+  for (var i = 0; i < node.children.length; i++) {
+  // recursively call getElementsByCLassName for each childnode
+    var childResults = getElementsByClassName(className, node.children[i]);
+    nodes = nodes.concat(childResults); // concat results meow
+  }
+  return nodes;
 };
